626899c5d89feb2c0da457e62c9e436a68fa4b76
Nthabi23 <monagengn2@gmail.com>
2023-11-16 01:33:17 +0200

    Create WeatherTemperature.js

---
+++ src/WeatherTemperature.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function WeatherTemperature(props) {
+    const[unit, setUnit] =useState('celsius');
+
+    function showFahrenheit(event) {
+        event.preventDefault();
+        setUnit("fahrenheit")
+    }
+
+    function showCelsius(event) {
+        event.preventDefault();
+        setUnit("fahrenheit")
+    }
+    
+    function fahrenheit () {
+        return (props.celsius * 9)/5 +32;
+    }
+    if (unit==="celsius") {
+        return(
+            <span className="WeatherTemperature">
+                <span className="Temperature">{Math.round(props.celsius)}</span> <span className="unit">&deg; | <a href="/" onCkick={showFahrenheit} className="text-decoration-none">&deg;F</a></span>
+            </span>
+        );
+    } else {
+        return(
+            <span className="WeatherTemperature">
+                <span className="temperature">{Math.round(fahrenheit())}</span> <span className="unit"> <a href="/" onClick={showCelsius} className="text-decoration-none" >&deg;C</a> | &deg;F</span>
+            </span>
+        );
+    }
+}
