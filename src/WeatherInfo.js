74648f717332444867838a1db44971778549374a
Nthabi23 <monagengn2@gmail.com>
2023-11-22 13:00:31 +0200

    update
    WeatherInfo.css styling and updating WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,37 +1,52 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
-import WeatherIcon from "./WeatherIcon";
+import "./WeatherInfo.css"
 import WeatherTemperature from "./WeatherTemperature";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 
 export default function WeatherInfo(props) {
-  return (
+  return(
     <div className="WeatherInfo">
-      <h1>{props.data.city}</h1>
-      <ul>
-        <li>
-          <FormattedDate date={props.data.date} />
-        </li>
-        <li className="text-capitalize">{props.data.description}</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="clearfix">
-            <div className="float-left">
-              <WeatherIcon code={props.data.icon} size={52} />
-            </div>
-
-            <div className="float-left">
-              <WeatherTemperature celsius={props.data.temperature} />
+      <Row>
+        <Col>
+        <h1>{props.city}</h1>
+        <ul>
+          <li className="text-capitalize">{props.data.description}</li>
+        </ul>
+        </Col>
+        <Col>
+        <ul>
+          <li>
+            <FormattedDate date={props.data.date} />
+          </li>
+        </ul>
+        </Col>
+      </Row>
+      <Row className="justify-content-center mt-3">
+        <Col className="weather-description col-sm-6">
+          <div className="row">
+            <div className="col-sm-4 justify-content-center icon-current">
+              <img
+              src={props.data.icon}
+              alt={props.data.description}
+              className="img-fluid weather-icon" />
             </div>
           </div>
-        </div>
-        <div className="col-6">
+        </Col>
+        <Col className="col-sm-6">
           <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind} km/h</li>
+            <li>
+              Humidity:
+              {props.data.humidity}%
+            </li>
+            <li>
+              Wind: {Math.round(props.data.wind * 3.6)} km/h
+            </li>
           </ul>
-        </div>
-      </div>
+        </Col>
+      </Row>
     </div>
   );
 }
